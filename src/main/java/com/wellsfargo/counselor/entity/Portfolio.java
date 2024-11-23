76c82a2a0f8beeb7c614a1397e4c48b9f2ea9cc8
niz-aws <nizrandom@gmail.com>
2024-11-22 20:47:57 -0500

    Implemented classes for each entity in the data model with relationships and full CRUD functionality

---
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -0,0 +1,54 @@
+package com.wellsfargo.counselor.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDate;
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue()
+    private long portfolioId;
+
+    @Column(nullable = false)
+    private LocalDate creationDate;
+
+    @ManyToOne
+    @JoinColumn(name = "clientId", nullable = false)
+    private Client client;
+
+    protected Portfolio() {
+
+    }
+
+    public Portfolio(LocalDate creationDate, Client client) {
+        this.creationDate = creationDate;
+        this.client = client;
+    }
+
+    public Long getPortfolioId() {
+        return portfolioId;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }   
+}
---
+++ src/main/java/com/wellsfargo/counselor/entity/Security.java
@@ -0,0 +1,102 @@
+package com.wellsfargo.counselor.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDate;
+
+@Entity
+public class Security {
+
+    @Id
+    @GeneratedValue()
+    private long securityId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private double purchasePrice;
+
+    @Column(nullable = false)
+    private LocalDate purchaseDate;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "portfolioId", nullable = false)
+    private Portfolio portfolio;
+
+    protected Security() {
+
+    }
+
+    public Security(String name, String category, double purchasePrice, LocalDate purchaseDate, int quantity, Portfolio portfolio) {
+        this.name = name;
+        this.category = category;
+        this.purchasePrice = purchasePrice;
+        this.purchaseDate = purchaseDate;
+        this.quantity = quantity;
+        this.portfolio = portfolio;
+    }
+
+    public Long getSecurityId() {
+        return securityId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(double purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public LocalDate getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(LocalDate purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Portfolio gePortfolio() {
+        return portfolio;
+    }
+
+    public void setPortfolio(Portfolio portfolio) {
+        this.portfolio = portfolio;
+    }
+}
